668d6a37ab53b411e98ac3f419dcd40fd73f4509
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-05-20 22:27:56 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,15 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const bntSubmit = document.querySelector('button[type="submit"]');
+const delayInput = document.querySelector('[name="delay"]');
+const stepInput = document.querySelector('[name="step"]');
+const amountInput = document.querySelector('[name="amount"]');
+
+
+bntSubmit.addEventListener("click", onSubmit);
+
+  const firstDelay = delayInput.value
+const delay = stepInput.value;
+const position = amountInput.value;
+
+  function onSubmit(event) {
+    event.preventDefault();
+    
+    const promises = amountInput.value
+   
+    const array = promises.map(promise => {
+      function createPromise(position, delay) {
+        const shouldResolve = Math.random() > 0.3;
+         setTimeout(() => 
+        if (shouldResolve) {
+          fulfilled(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        } else {
+          rejected(`❌ Rejected promise ${position} in ${delay}ms`)
+        }
+      } 
+    });
+   
 }
 
 
-function createPromise(position, delay) {
-  console.log("Helloy");
-}
 
-console.log('Helloy');
+
+
+
